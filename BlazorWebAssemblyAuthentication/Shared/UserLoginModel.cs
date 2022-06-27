e84a82c2816df45ffbc4e4f533c627afeb0f11de
Neal Robben <neal.robben@gmail.com>
2022-06-27 23:02:59 +0200

    Added UserLogin component

---
+++ BlazorWebAssemblyAuthentication/Shared/UserLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssemblyAuthentication.Shared
+{
+    public class UserLoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
