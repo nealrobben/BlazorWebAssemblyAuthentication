3825bf0c047ec5ad9b975f49d0e2388394b85dd0
Neal Robben <neal.robben@gmail.com>
2022-07-01 21:42:27 +0200

    Fixed build errors

---
+++ BlazorWebAssemblyAuthentication/Server/Controllers/UserLoginController.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWebAssemblyAuthentication.Server.Models;
+using BlazorWebAssemblyAuthentication.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebAssemblyAuthentication.Server.Controllers
+{
+    [ApiController]
+    public class UserLoginController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserLoginController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserLoginModel loginModel)
+        {
+            //var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+            return null;
+        }
+    }
+}
